1db2ef1750f0585e113f69db23b251c28af9b23d
Ben <general@simplexwebsites.com>
2022-02-23 19:54:30 -0500

    Continue work - spelling correct works; Work on making it multiprocessed.

---
+++ lib/spellcheck.py
@@ -0,0 +1,17 @@
+from spellchecker import SpellChecker
+if __name__ == "main":
+    raise Exception("This file is not intended to be run directly")
+
+def spellcheck(text):
+    # lowercase the text (even though the vectorizer will do it later)
+    text = text.lower()
+    spell = SpellChecker()
+    # get unknown words
+    misspelled = spell.unknown(text.split())
+    # loop through text (by space)
+    for word in (text.split()):
+        if word in misspelled:
+            # if word is misspelled, replace it with correct word
+            text = text.replace(word, spell.correction(word))
+            print(text)
+    return text
